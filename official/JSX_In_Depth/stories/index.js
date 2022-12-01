306cb5a5f97c98faf4cfccefb1ab9f392d663ff8
zhaojian@clog.com <250529492@qq.com>
2022-12-01 15:55:04 +0800

    feat: 添加图片

---
+++ official/JSX_In_Depth/stories/index.js
@@ -0,0 +1,18 @@
+// export function VideoStory (props) {
+//     return <h1>VideoStory, {props.story}</h1>
+// }
+
+
+// export function PhotoStory (props) {
+//     return <h1>PhotoStory, {props.story}</h1>
+// }
+
+
+function VideoStory (props) {
+    return <h1>VideoStory, {props.story}</h1> 
+}
+
+
+function PhotoStory (props) {
+    return <h1>PhotoStory, {props.story}</h1>
+}
